4dcef9051590d3a97e49189f2a6bda22f5909e55
Tsubasa Ishihara <tishihar@c6r4s2.42tokyo.jp>
2025-01-16 16:24:53 +0900

    init

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 18:59:32 by tishihar          #+#    #+#             */
-/*   Updated: 2025/01/16 14:02:43 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/01/16 16:21:38 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "libft.h"
 # include "get_next_line.h"
 # include "mlx.h"
-// # include "stdbool.h"
+
 
 
 
