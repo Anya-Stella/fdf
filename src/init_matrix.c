02983dae4fb567b76518e17c00f137907da37485
Tsubasa Ishihara <tishihar@c6r3s4.42tokyo.jp>
2025-01-17 20:21:45 +0900

    temp

--- src/init_matrix.c
+++ src/init_matrix.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 17:28:20 by tishihar          #+#    #+#             */
-/*   Updated: 2025/01/17 20:04:55 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/01/17 20:19:58 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ static	int	**create_matrix(int width, int height)
 	int	row;
 	int allocated;
 
-	matrix = (int **)ft_calloc(height, sizeof(int *));// 000000
+	matrix = (int **)ft_calloc(height + 1, sizeof(int *));// 000000
 	if (!matrix)
         return (NULL);
 	temp = matrix;
@@ -54,7 +54,7 @@ static	int	**create_matrix(int width, int height)
 	allocated = 0;
 	while (row--)
 	{
-		*temp = (int *)ft_calloc(width, sizeof(int));
+		*temp = (int *)ft_calloc(width + 1, sizeof(int));
 		if (!*temp)
 		{
 			interruption_create_matrix(allocated, matrix);
@@ -67,16 +67,31 @@ static	int	**create_matrix(int width, int height)
 }
 
 
+
+
 bool	set_matrix(char *file_name, t_fdf *data_)
 {
 	int	**matrix;
+	int	**matrix_dummy;
+	int	fd;
+	char *line;
 
 	// 二次元配列を確保する
 	matrix = create_matrix(data_->width, data_->height);
-
-
-	
-
+	matrix_dummy = matrix;
 
 	// 二次元配列に登録する
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+		return (false);
+	
+	while (!!(line = get_next_line(fd)))
+	{
+		// この段階でline = "0 10 2 34  4  3 3 3 3  \0 \n \0"
+		// 入力：lineと書き込むためにmatrixのポインタを渡す
+
+		// 1: 配列に書き込む
+		//２：
+		fill_array(line, &matrix);
+	}
 }
